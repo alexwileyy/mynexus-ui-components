a4296c9570e9150d0b3eebe297ca4ce3ce3642e2
Alex Wiley <alex.wiley@oneupsales.co.uk>
2020-05-13 15:22:58 +0100

    Welcome box with accordion text completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const { series, src, dest, task, watch } = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss')
 
 sass.compiler = require('node-sass');
 
 task('sass', function () {
   return src('./styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('./public/css'));
 });
 
